9d1f9dbf2faed18227ad86accdd0066df83e2e38
Tom <tommykaitocrypto@gmail.com>
2026-06-11 14:33:35 +0200

    Fix api/notion.js: use CommonJS export (consistent with other API routes)

--- api/notion.js
+++ api/notion.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   const dbId = req.query.db;
   if (!dbId) {
     return res.status(400).json({ error: "Missing ?db= parameter" });
